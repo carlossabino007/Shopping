fe40f02968aae37f8c50b77fb3cee55227621e0e
carlossabino007 <carlossabino007@gmail.com>
2022-08-06 13:07:06 -0500

    Finish Countries CRUD

---
+++ Shopping/Shopping/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shopping.Data.Entities;
+
+namespace Shopping.Data
+{
+    public class DataContext: DbContext
+    {
+        /* le paso el objeto creando options al constructor de la clase padre */
+        /* ,al base,al DbContext */
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+
+        {
+        }
+
+        public DbSet<Country> countries  { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
---
+++ Shopping/Shopping/Data/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+            /* {0} se reemplaza por País del Name. El campo País es obligatorio */ 
+        [Required(ErrorMessage ="El campo {0} es Requerido")]
+        [Display(Name ="País")]
+            /* {0} país     {1} caracteres */
+        [MaxLength(50, ErrorMessage ="El campo {0} debe tener Máximo {1} caracteres") ]
+
+        public string Name { get; set; }
+
+    }
+}
